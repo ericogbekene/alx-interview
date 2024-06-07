827e2e35b73dbdb7133177ac10c88616e89ce0d3
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-07 01:19:02 +0100

    trying to solve the lockbox problem

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+module to solve the lockboxes challenge
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    trying to unlock all boxes
+    """
+    open_boxes = [True] + [False] * (len(boxes) - 1)
+
+    for box in boxes[1:]:
+        for key in box:
+            if key > 0 and key < len(open_boxes) and not open_boxes[key]:
+                open_boxes[key] = True
+
+    return all(open_boxes)
