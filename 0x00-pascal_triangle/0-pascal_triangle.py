b85c3c543cce5e9baee77245560ee972628eb441
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-14 16:56:13 +0100

    updating pascals triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 module to print pascal triangle
 """
+
 
 def print_pascal(list):
     """
@@ -11,6 +12,7 @@ def print_pascal(list):
         for index in line:
             print(index)
 
+
 def pascal_triangle(n):
     """
     this funtion will print the pascal triangle, n lines
@@ -18,16 +20,15 @@ def pascal_triangle(n):
     if (n < 0):
         return []
 
-    results_array = [[0 for x in range(n)] 
-              for y in range(n)] 
+    results = [[0 for x in range(n)]
+                     for y in range(n)]
     for line in range(0, n):
         for row in range(0, line + 1):
             if row == 0 or row == line:
-                results_array[line][row] = 1
+                results[line][row] = 1
                 # print(results_array[line][row])
                 # print("")
             else:
-                results_array[line][row] = results_array[line-1][row] + results_array[line-1][row + 1]
+                results[line][row] = results[line-1][row] + results[line-1][row + 1]
     # print_pascal(results_array)
-    return results_array
-
+    return results
