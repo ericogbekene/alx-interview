782f0f434cc2d24a4c84c886bb3558763a8f1508
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-14 17:18:13 +0100

    still fixing this pascal triangle inner calcullation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,18 +17,18 @@ def pascal_triangle(n):
     """
     this funtion will print the pascal triangle, n lines
     """
-    if (n < 0):
+    if (n <= 0):
         return []
 
     results = [[0 for x in range(n)]
                for y in range(n)]
     for line in range(0, n):
-        for i in range(0, line):
+        for i in range(0, line + 1):
             if i == 0 or i == line:
                 results[line][i] = 1
                 # print(results_array[line][row])
                 # print("")
             else:
-                results[line][i] = results[line-1][i - 1] + results[line-1][i]
+                results[line][i] = results[line - 1][i - 1] + results[line-1][i]
     # print_pascal(results_array)
     return results
