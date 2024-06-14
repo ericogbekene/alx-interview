1fad7d83a3651e726bec8614b25cc2c52ccad978
Eric Ogbekene <ericogbekene@icloud.com>
2024-06-14 17:03:05 +0100

    fixing pascals triangle output

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -21,7 +21,7 @@ def pascal_triangle(n):
         return []
 
     results = [[0 for x in range(n)]
-                     for y in range(n)]
+               for y in range(n)]
     for line in range(0, n):
         for row in range(0, line + 1):
             if row == 0 or row == line:
@@ -29,6 +29,6 @@ def pascal_triangle(n):
                 # print(results_array[line][row])
                 # print("")
             else:
-                results[line][row] = results[line-1][row] + results[line-1][row + 1]
+                results[line][row] = results[line-1][row - 1] + results[line-1][row]
     # print_pascal(results_array)
     return results
